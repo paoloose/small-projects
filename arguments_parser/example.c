e43a313d4e59cf5a4079f1e14b4c937ddb80023e
leflores <pflores.fisi22@gmail.com>
2023-01-14 02:13:45 -0500

    docs(c): add error example

--- arguments_parser/example.c
+++ arguments_parser/example.c
@@ -14,17 +14,20 @@ int main(int argc, char *argv[]) {
     printf("argument: <port>: %s\n", port);
 
     if (isflagset("--ipv4")) {
-        printf("--ipv4 flag is set\n");
+        printf("'--ipv4' flag is set\n");
     }
     if (isflagset("--ipv6")) {
-        printf("--ipv6 flag is set\n");
+        printf("'--ipv6' flag is set\n");
     }
     if (isflagset("--ttl")) {
-        printf("--ttl flag is set\n");
-        printf("--ttl value: %s\n", getarg("--ttl"));
+        printf("'--ttl' flag is set (value: %s)\n", getarg("--ttl"));
     }
     getarg("--non-existing"); // returns NULL
     isflagset("--non-existing"); // returns false (0)
+
+    // getarg("--ipv4");
+    // exit with error message
+    // "fatal error: attempt to get value of boolean flag --ipv4"
 
     freearguments(); // free memory allocated by parseargs()
     return 0;
